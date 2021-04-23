a6f30e1052b535ed6d5e125603563d00828bf609
alexismarroquin7 <alexismarroquin754@gmail.com>
2021-04-23 07:55:19 -0700

    [POST] /api/auth/login tests

--- api/server.test.js
+++ api/server.test.js
@@ -37,9 +37,20 @@ describe('server', () => {
       expect(res.body).toMatchObject({ id: 1, username: johnDoe, password: "hashed pass" });
     }, 500);
   });
-  // describe('[POST] /api/auth/login', () => {
-
-  // });
+  describe('[POST] /api/auth/login', () => {
+    it('on FAILED login due to `username` or `password` missing, the response body should include { "message": "username and password required" }', async () => {
+      const res = await request(server).post('/api/auth/login').send({});
+      expect(res.body).toMatchObject({ message: "username and password required" });
+    });
+    it('on FAILED login due to `username` not existing in the db or `password` being incorrect, the response body should include { "message": "invalid credentials" }', async () => {
+      const res = await request(server).post('/api/auth/login').send(johnDoe);
+      expect(res.body).toMatchObject({ message: "invalid credentials" });
+    });
+    it('on SUCCESSFUL login the response body should have `message` and `token` { "message": "welcome, johnDoe", "token": "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8" }', async () => {
+      const res = await request(server).post('/api/auth/login').send(johnDoe);
+      expect(res.body).toMatchObject({ message: "welcome, johnDoe", token: "eyJhbGciOiJIUzI ... ETC ... vUPjZYDSa46Nwz8" });
+    });
+  });
   // describe('[GET] /api/jokes', () => {
 
   // });
